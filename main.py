d4775a05f8e826b569316b58edae71d22b0976b3
Jerem <silva.jeremie93@gmail.com>
2022-11-04 09:55:53 +0100

    DOCS and STYLE : main.py

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import requests
 from bs4 import BeautifulSoup  # type: ignore
 
 
-URL_MAIN = "https://books.toscrape.com"
-
+URL_MAIN:str = "https://books.toscrape.com"
+PATH_URL_CATEGORY: str = "https://books.toscrape.com/catalogue"
+URL_META_CATEGORY_BOOK: str = "https://books.toscrape.com/catalogue/category/books_1/index.html"
+PATH_URL_META_CATEGORY: str = "https://books.toscrape.com/catalogue/category"
 
 class Book:
-    def __init__(self,
-                 url_product: str):
+    """Cette class permet de scrapper le contenu d'une page livre,
+    et inscremete le self de chaque instance avec les valeurs scrappées.
+    """
+    def __init__(self, url_product: str):
+    # Initialisation de l'instance à partir de l'url du livre passé en parametre
         # Récupérer et perser le html de la page produit
         soup = BeautifulSoup(requests.get(url_product).content, "html.parser")
         self.product_page_url = url_product
@@ -31,7 +36,7 @@ class Book:
         self.title_book = soup.find("h1").contents[0]
 
     def catch_product_description(self, soup: BeautifulSoup) -> None:
-        # On evite que le programme ne bloque si la description est vide
+    # On evite que le programme ne bloque si la description est vide
         x = soup.select("article.product_page > p")
         if x == []:
             self.product_description = x
@@ -42,28 +47,34 @@ class Book:
 
 
 class Category_book:
+    """Cette classe permet de scrapper les urls des livres sur les pages catégorie,
+    en tenant compte de l'éventuelle pagination,
+    et incrémente une liste avec les valeurs.
+    """
     def __init__(self, url_category: str):
+    # Initialisation de l'instance à partir de l'url de la catégorie passé en parametre
         soup = BeautifulSoup(requests.get(url_category).content, "html.parser")
-        # Liste contenant toutes les URLs de la pagination
+        # Liste éventuellement incrementée par toutes les URLs de la pagination
         self.urls_category = [url_category]
-        self.list_urls_livres: list[str] = []
+        self.list_urls_books: list[str] = []
         self.catch_urls_books(soup)
         self.heading = soup.find("h1").contents[0]
 
     def catch_urls_books(self, soup: BeautifulSoup) -> None:
-        # Recupere tous les liens placés sur des <h3> dans le corps de la page
+    # Permet de scrapper une page de catégorie et incrémente la liste des urls de cette catégorie
+        # Récupere tous les liens placés sur des balises <h3> dans le corps de la page
         for i in range(0, len(soup.select("ol.row h3 a"))):
             x = soup.select("ol.row h3 a")[i].get("href").split("/")
             del x[0:2]
-            x[0] = "https://books.toscrape.com/catalogue"
-            self.list_urls_livres.append("/".join(x))
+            x[0] = PATH_URL_CATEGORY
+            self.list_urls_books.append("/".join(x))
         # Verifie si il y a un bouton de pagination
         if soup.find("li", class_="next"):
             self.pagination_category(soup)
         return None
 
     def pagination_category(self, soup: BeautifulSoup) -> None:
-        # Recupere l'URL de la prochaine page de pagination et la rajoute dans la liste des URLs
+    # Recupere l'URL de la prochaine page de pagination et la rajoute dans la liste des URLs de catégorie
         url_category_split = self.urls_category[-1].split("/")
         url_category_split[-1] = soup.select("li.next a")[0].get("href")
         self.urls_category.append("/".join(url_category_split))
@@ -75,8 +86,12 @@ class Category_book:
 
 
 class BooksToScrape_category:
+    """Cette class permet de scrapper les urls des catégories de livres du site BookToScrape,
+    et incrémente une liste avec les valeurs.
+    """
     def __init__(self) -> None:
-        self.url_parent = "https://books.toscrape.com/catalogue/category/books_1/index.html"
+    # Initialisation de l'instance à partir de l'url META de la catégorie
+        self.url_parent = URL_META_CATEGORY_BOOK
         soup = BeautifulSoup(requests.get(
             self.url_parent).content, "html.parser")
         self.list_all_category_urls: list[str] = []
@@ -84,26 +99,33 @@ class BooksToScrape_category:
         print(f"Total : {len(self.list_all_category_urls)} category")
 
     def catch_urls_category(self, soup: BeautifulSoup) -> None:
-        # Recuperer l'URL de chaque categories et incrementer la liste
+    # Recuperer l'URL de chaque categories et incrementer une liste avec les valeurs
         for i in range(0, len(soup.select("div.side_categories > ul.nav > li > ul > li > a"))):
             x = soup.select("div.side_categories > ul.nav > li > ul > li > a")[
                 i].get("href").split("/")
-            x[0] = "https://books.toscrape.com/catalogue/category"
+            x[0] = PATH_URL_META_CATEGORY
             self.list_all_category_urls.append("/".join(x))
         return None
 
 
 # --------------------------PROGRAMME--------------------------
 
-
+# Récupere la liste des catégories de livres
 super_category_book = BooksToScrape_category()
 print()
+
+# Premiere boucle qui parcours la liste des urls des catégories
 for i in range(0, len(super_category_book.list_all_category_urls)):
+    # Créer une instance de chaque catégorie en lui passant l'url de la catégorie en parametre
     x = Category_book(super_category_book.list_all_category_urls[i])
-    print(f"OK -- {x.heading} : {len(x.list_urls_livres)} books")
-    for u in range(0, len(x.list_urls_livres)):
-        y = Book(x.list_urls_livres[u])
+    print(f"OK -- {x.heading} : {len(x.list_urls_books)} books")
+
+    # Deuxieme boucle qui cible la catégorie en cours et qui parcours sa liste d'url de livres
+    for u in range(0, len(x.list_urls_books)):
+        # Chaque page de livre est scrappée
+        y = Book(x.list_urls_books[u])
         print(f"{u+1} >> {y.title_book}")
+    
     print()
 
 
@@ -111,9 +133,10 @@ for i in range(0, len(super_category_book.list_all_category_urls)):
 #    csv.writer(csvfile).writerow("ergrdgbrfsefsef, fefe, fesfs")
 
 
+
+
+
 """
    VSC : retour à la ligne '\n' + virgules pour changer de cellule
    https://www.scrapingbee.com/blog/download-image-python/
-   Paramétrer les class pour qu'elles soient parentes ?
-
 """
